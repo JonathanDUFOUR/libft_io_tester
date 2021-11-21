48262faaa06314911ecef2f5b88e9871cafa3193
Barbara CANO <bcano@e2r6p7.clusters.42paris.fr>
2021-11-21 19:11:07 +0100

    Write tests for is_ascii functions [OK]

--- srcs/test/test_ft_isascii.c
+++ srcs/test/test_ft_isascii.c
@@ -6,10 +6,11 @@
 /*   By: bcano <bcano@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 23:10:23 by jodufour          #+#    #+#             */
-/*   Updated: 2021/11/21 18:02:09 by bcano            ###   ########.fr       */
+/*   Updated: 2021/11/21 18:55:51 by bcano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
 #include <stdio.h>
 #include "ft_io.h"
 #include "tester.h"
@@ -20,19 +21,17 @@ typedef struct s_test	t_test;
 struct	s_test
 {
 	int const	num;
-	unsigned const	test;
-	int const	expect;
+	unsigned long const	test;
 };
 
 static t_test const		g_test[] = {
-{0, -66, 0},
-{1, 128, 0},
-{2, 90, 1},
-{3, 2147483757, 0},
-{4, 120, 1},
+{0, -66},
+{1, 128},
+{2, 90},
+{3, 2147483757},
+{4, 120},
 {0}
 };
-
 
 int	test_ft_isascii(int *const ret)
 {
@@ -43,7 +42,7 @@ int	test_ft_isascii(int *const ret)
 	while (g_test[i].test)
 	{
 		result(g_test[i].num,
-			ft_isascii(g_test[i].test) == g_test[i].expect);
+			ft_isascii(g_test[i].test) == isascii(g_test[i].test));
 		i++;
 	}
 	printf("\n");
