0c5917e00e1f15a192c4a3a329017bf84b43b024
Barbara CANO <bcano@e2r6p7.clusters.42paris.fr>
2021-11-21 18:12:38 +0100

    Write tests for is_digit functions [OK]

--- objs/err_msg.d
+++
@@ -1,8 +0,0 @@
-objs/err_msg.o: srcs/err_msg.c ../include/ft_colors.h \
-  private/err_msg_lookup.h private/enum/e_ret.h
-
-../include/ft_colors.h:
-
-private/err_msg_lookup.h:
-
-private/enum/e_ret.h:
--- objs/main.d
+++
@@ -1,8 +0,0 @@
-objs/main.o: srcs/main.c private/tester.h private/test_lookup.h \
-  private/enum/e_ret.h
-
-private/tester.h:
-
-private/test_lookup.h:
-
-private/enum/e_ret.h:
