3e39b13c7ac5fec8f97b973f84b746ea1df950a9
Barbara CANO <bcano@e2r6p7.clusters.42paris.fr>
2021-11-21 18:04:31 +0100

    Write tests for is_ascii functions [OK]

--- srcs/test/test_ft_isascii.c
+++ srcs/test/test_ft_isascii.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_ft_isascii.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bcano <bcano@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 23:10:23 by jodufour          #+#    #+#             */
-/*   Updated: 2021/11/21 08:38:25 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/11/21 18:02:09 by bcano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,37 @@
 #include "tester.h"
 #include "enum/e_ret.h"
 
+typedef struct s_test	t_test;
+
+struct	s_test
+{
+	int const	num;
+	unsigned const	test;
+	int const	expect;
+};
+
+static t_test const		g_test[] = {
+{0, -66, 0},
+{1, 128, 0},
+{2, 90, 1},
+{3, 2147483757, 0},
+{4, 120, 1},
+{0}
+};
+
+
 int	test_ft_isascii(int *const ret)
 {
+	int	i;
+
 	printf("%20s:", __func__ + 5);
+	i = 0;
+	while (g_test[i].test)
+	{
+		result(g_test[i].num,
+			ft_isascii(g_test[i].test) == g_test[i].expect);
+		i++;
+	}
 	printf("\n");
 	return (*ret = SUCCESS);
 }
